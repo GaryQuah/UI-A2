db2e7db06aa394d130ae49a84bb3abc4a8b07819
GaryQuah <garyquah13@gmail.com>
2020-01-13 15:30:53 +0800

    updated

---
+++ Assets/Scripts/GameplayLoadingScripts/LoadingBar.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadingBar : MonoBehaviour
+{
+    private Slider loadingBar;
+    private float progress = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        loadingBar = GetComponent<Slider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (progress < 1.0f)
+            progress += 0.01f;
+
+        loadingBar.value = progress;
+    }
+}
