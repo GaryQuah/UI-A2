1746986bf0011f0c04fd86267f2905b7bdc4a046
Keldop <42366824+Keldop@users.noreply.github.com>
2020-02-01 01:26:16 +0800

    Just in case i lose files

---
+++ Assets/Scripts/UserInfo.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UserInfo : MonoBehaviour
+{
+    public string Username;
+    public string Password;
+    public int Level;
+    public int DiamondCount;
+
+    //Car
+    public int CarType;
+    public int WheelType;
+
+    //Setters
+    public void SetUsername(string name)
+    {
+        Username = name;
+    }
+    public void SetPassword(string password)
+    {
+        Password = password;
+    }
+    public void SetCarType(int cartype)
+    {
+        CarType = cartype;
+    }
+    public void SetWheelType(int wheeltype)
+    {
+        WheelType = wheeltype;
+    }
+    public void SetDiamondCount(int DC)
+    {
+        DiamondCount = DC;
+    }
+    public void SetLevel(int level)
+    {
+        Level = level;
+    }
+    //Getters
+    public string GetUserName()
+    {
+        return Username;
+    
+    }
+    public string GetPassword()
+    {
+        return Password;
+    }
+    public int GetCarType()
+    {
+        return CarType;
+    }
+    public int GetWheelType()
+    {
+        return WheelType;
+    }
+    public int GetLevel()
+    {
+        return Level;
+    }
+    public int GetDiamondCount()
+    {
+        return DiamondCount;
+    }
+}
