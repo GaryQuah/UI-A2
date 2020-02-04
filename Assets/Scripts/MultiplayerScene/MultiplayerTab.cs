2e5194ed914f00e06e15db3f50895815bf3da391
Keldop <42366824+Keldop@users.noreply.github.com>
2020-02-04 19:38:12 +0800

    Multiplayer scene updated customisationscene + singleplayer

--- Assets/Scripts/MultiplayerScene/MultiplayerTab.cs
+++ Assets/Scripts/MultiplayerScene/MultiplayerTab.cs
@@ -1,18 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MultiplayerTab : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] Button InviteBtn;
+    [SerializeField] GameObject LobbyTab;
+    [SerializeField] GameObject InvitationTab;
+    [SerializeField] Button InvitationTabBackBtn;
+
     void Start()
+    {
+        InviteBtn.onClick.AddListener(InviteButtonEvent);
+        InvitationTabBackBtn.onClick.AddListener(InvitationTabBackButtonEvent);
+
+        InvitationTab.SetActive(false);
+    }
+
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void InviteButtonEvent()
     {
-        
+        InvitationTab.SetActive(true);
+        LobbyTab.SetActive(false);
+    }
+
+    public void InvitationTabBackButtonEvent()
+    {
+        LobbyTab.SetActive(true);
+        InvitationTab.SetActive(false);
     }
 }
--- Assets/Scripts/SinglePlayerScene/TabChanger.cs
+++ Assets/Scripts/SinglePlayerScene/TabChanger.cs
@@ -5,11 +5,34 @@ using UnityEngine.UI;
 
 public class TabChanger : MonoBehaviour
 {
-    int Tab;
+    [SerializeField] Button StoryBtn;
+   
+    [SerializeField] Button TimetrialBtn;
+
+    [SerializeField] Button Level1Btn;
+    [SerializeField] Button Level2Btn;
+    [SerializeField] Button Level3Btn;
+    [SerializeField] Button Level4Btn;
+
+    [SerializeField] GameObject Map;
+    //Sprite
+    [SerializeField] Sprite NotSelectedSprite;
+    [SerializeField] Sprite SelectedSprite;
+
+    [SerializeField] Sprite Map1;
+    [SerializeField] Sprite Map2;
+    [SerializeField] Sprite Map3;
+    [SerializeField] Sprite Map4;
 
     void Start()
     {
-        
+        StoryBtn.onClick.AddListener(StoryButtonEvent);
+        TimetrialBtn.onClick.AddListener(TimeTrialButtonEvent);
+
+        Level1Btn.onClick.AddListener(Level1ButtonEvent);
+        Level2Btn.onClick.AddListener(Level2ButtonEvent);
+        Level3Btn.onClick.AddListener(Level3ButtonEvent);
+        Level4Btn.onClick.AddListener(Level4ButtonEvent);
     }
 
     private void Update()
@@ -17,8 +40,52 @@ public class TabChanger : MonoBehaviour
         
     }
 
-    public void SetActive()
+    public void StoryButtonEvent()
     {
+        StoryBtn.GetComponent<Image>().sprite = SelectedSprite;
+        TimetrialBtn.GetComponent<Image>().sprite = NotSelectedSprite;
+    }
 
+    public void TimeTrialButtonEvent()
+    {
+        TimetrialBtn.GetComponent<Image>().sprite = SelectedSprite;
+        StoryBtn.GetComponent<Image>().sprite = NotSelectedSprite;
+    }
+
+    public void Level1ButtonEvent()
+    {
+        Level1Btn.GetComponent<Image>().sprite = SelectedSprite;
+        Level2Btn.GetComponent<Image>().sprite = NotSelectedSprite;
+        Level3Btn.GetComponent<Image>().sprite = NotSelectedSprite;
+        Level4Btn.GetComponent<Image>().sprite = NotSelectedSprite;
+
+        Map.GetComponent<Image>().sprite = Map1;
+    }
+    public void Level2ButtonEvent()
+    {
+        Level2Btn.GetComponent<Image>().sprite = SelectedSprite;
+        Level1Btn.GetComponent<Image>().sprite = NotSelectedSprite;
+        Level3Btn.GetComponent<Image>().sprite = NotSelectedSprite;
+        Level4Btn.GetComponent<Image>().sprite = NotSelectedSprite;
+
+        Map.GetComponent<Image>().sprite = Map2;
+    }
+    public void Level3ButtonEvent()
+    {
+        Level3Btn.GetComponent<Image>().sprite = SelectedSprite;
+        Level1Btn.GetComponent<Image>().sprite = NotSelectedSprite;
+        Level2Btn.GetComponent<Image>().sprite = NotSelectedSprite;
+        Level4Btn.GetComponent<Image>().sprite = NotSelectedSprite;
+
+        Map.GetComponent<Image>().sprite = Map3;
+    }
+    public void Level4ButtonEvent()
+    {
+        Level4Btn.GetComponent<Image>().sprite = SelectedSprite;
+        Level1Btn.GetComponent<Image>().sprite = NotSelectedSprite;
+        Level2Btn.GetComponent<Image>().sprite = NotSelectedSprite;
+        Level3Btn.GetComponent<Image>().sprite = NotSelectedSprite;
+
+        Map.GetComponent<Image>().sprite = Map4;
     }
 }
