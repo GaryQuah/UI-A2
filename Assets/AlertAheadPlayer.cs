683a617929c4b16a1e3196e4db95c11313b0e4d1
GaryQuah <garyquah13@gmail.com>
2020-02-06 00:01:42 +0800

    update gameplay

---
+++ Assets/AlertAheadPlayer.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class AlertAheadPlayer : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI textOne;
+    [SerializeField] TextMeshProUGUI textTwo;
+
+    [SerializeField] GameObject player;
+    [SerializeField] GameObject enemyAI;
+
+    private GameObject objectivesTrackerObject;
+
+    private ObjectivesTracker objectivesTracker;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        objectivesTrackerObject = GameObject.Find("ObjectivesTracker");
+
+        objectivesTracker = objectivesTrackerObject.GetComponent<ObjectivesTracker>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int distanceBetweenPlayers = (int)Vector3.Distance(enemyAI.transform.position, player.transform.position) * 10;
+
+        if (distanceBetweenPlayers <= 500)
+        {
+            if (objectivesTracker.getCurrentPosition() == 1)
+            {
+                textOne.text = "2nd ";
+                textTwo.text = "2nd ";
+            }
+            else
+            {
+                textOne.text = "1st ";
+                textTwo.text = "1st ";
+            }
+
+            textOne.text += distanceBetweenPlayers.ToString() + "m";
+            textTwo.text += distanceBetweenPlayers.ToString() + "m";
+        }
+        else
+        {
+            textOne.text = "";
+            textTwo.text = "";
+        }
+    }
+}
---
+++ Assets/AlertPlayerScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class AlertPlayerScript : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI textOne;
+    [SerializeField] TextMeshProUGUI textTwo;
+    [SerializeField] GameObject alertImage;
+
+    [SerializeField] GameObject player;
+    [SerializeField] GameObject enemyAI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.SetActive(false);
+        alertImage.SetActive(false);
+        textOne.gameObject.SetActive(false);
+        textTwo.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int distanceBetweenPlayers = (int)Vector3.Distance(enemyAI.transform.position, player.transform.position) * 10;
+
+        if (distanceBetweenPlayers <= 500)
+        {
+            alertImage.SetActive(true);
+            textOne.gameObject.SetActive(true);
+            textTwo.gameObject.SetActive(true);
+
+            textOne.text = distanceBetweenPlayers.ToString() + " meters";
+            textTwo.text = distanceBetweenPlayers.ToString() + " meters";
+        }
+        else
+        {
+            alertImage.SetActive(false);
+            textOne.gameObject.SetActive(false);
+            textTwo.gameObject.SetActive(false);
+            gameObject.SetActive(false);
+        }
+    }
+}
