aa14022d5ecd4ac39ff78baa0d48cbfa96b7e7c9
Keldop <42366824+Keldop@users.noreply.github.com>
2020-02-06 02:49:12 +0800

    Update CustomisationScene

--- Assets/Scripts/CustomisationScene/Customisation.cs
+++ Assets/Scripts/CustomisationScene/Customisation.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class Customisation : MonoBehaviour
 {
-
+    //Tab
     [SerializeField] Button CarBtn;
     [SerializeField] Button WheelBtn;
     [SerializeField] Button EngineBtn;
@@ -13,11 +14,49 @@ public class Customisation : MonoBehaviour
     [SerializeField] Sprite NotSelectedSprite;
     [SerializeField] Sprite SelectedSprite;
 
+    //
+    [SerializeField] Button RightArrowBtn;
+    [SerializeField] Button LeftArrowBtn;
+
+    [SerializeField] Button EquipBtn;
+    [SerializeField] Button BuyBtn;
+    [SerializeField] TextMeshProUGUI BuyText;
+
+    [SerializeField] GameObject Equiped;
+    [SerializeField] GameObject Owned;
+
+    [SerializeField] GameObject CarImage;
+    //Sprites
+    [SerializeField] Sprite Car1;
+    [SerializeField] Sprite Car2;
+    [SerializeField] Sprite Car3;
+    [SerializeField] Sprite Car4;
+
+    [SerializeField] Sprite Wheel1;
+    [SerializeField] Sprite Wheel2;
+    [SerializeField] Sprite Wheel3;
+    [SerializeField] Sprite Wheel4;
+    
+    [SerializeField] Sprite Engine1;
+    [SerializeField] Sprite Engine2;
+    [SerializeField] Sprite Engine3;
+    [SerializeField] Sprite Engine4;
+
+    // CarStats
+
+
+    UserInfo user = new UserInfo();
+    int Diamondcost;
+    int i = 0;
     void Start()
     {
         CarBtn.onClick.AddListener(CarButtonEvent);
         WheelBtn.onClick.AddListener(WheelButtonEvent);
         EngineBtn.onClick.AddListener(EngineButtonEvent);
+
+        RightArrowBtn.onClick.AddListener(RightArrowButtonEvent);
+        LeftArrowBtn.onClick.AddListener(LeftArrowButtonEvent);
+        BuyBtn.onClick.AddListener(BuyButtonEvent);
     }
 
     void Update()
@@ -45,5 +84,133 @@ public class Customisation : MonoBehaviour
         WheelBtn.GetComponent<Image>().sprite = NotSelectedSprite;
         CarBtn.GetComponent<Image>().sprite = NotSelectedSprite;
     }
+    public void BuyButtonEvent()
+    {
+        if (user.DiamondCount >= Diamondcost)
+        {
+            user.DiamondCount = user.DiamondCount - Diamondcost;
+        }
+    }
+    public void RightArrowButtonEvent()
+    {
+        Owned.gameObject.SetActive(false);
+        Equiped.gameObject.SetActive(false);
+        EquipBtn.gameObject.SetActive(false);
+        BuyBtn.gameObject.SetActive(false);
+        i++;
+        if (i > 3)
+            i = 0;
 
-}
+        bool b_Owned = false;
+
+        for(int j = 0; j < user.OwnedCar.Length; ++j)
+        {
+            if(user.OwnedCar[j] == i)
+            {
+                Owned.gameObject.SetActive(true);
+                b_Owned = true;
+                if (user.CarType != i)
+                {
+                    EquipBtn.gameObject.SetActive(true);
+                }
+                break;
+            }
+        }
+        if (!b_Owned)
+        {
+            BuyBtn.gameObject.SetActive(true);
+        }
+        if (user.CarType == i)
+        {
+            Equiped.gameObject.SetActive(true);
+        }
+        
+        switch (i)
+        {
+            case 0:
+                CarImage.GetComponent<Image>().sprite = Car1;
+                Diamondcost = 35;
+                break;
+            case 1:
+                CarImage.GetComponent<Image>().sprite = Car2;
+                Diamondcost = 45;
+                break;
+            case 2:
+                CarImage.GetComponent<Image>().sprite = Car3;
+                Diamondcost = 55;
+                break;
+            case 3:
+                CarImage.GetComponent<Image>().sprite = Car4;
+                Diamondcost = 205;
+                break;
+        }
+        if (BuyBtn.enabled)
+            SetTextColor();
+    }
+    public void LeftArrowButtonEvent()
+    {
+        Owned.gameObject.SetActive(false);
+        Equiped.gameObject.SetActive(false);
+        EquipBtn.gameObject.SetActive(false);
+        BuyBtn.gameObject.SetActive(false);
+        i--;
+        if (i < 0)
+            i = 3;
+
+        bool b_Owned = false;
+
+        for (int j = 0; j < user.OwnedCar.Length; ++j)
+        {
+            if (user.OwnedCar[j] == i)
+            {
+                Owned.gameObject.SetActive(true);
+                b_Owned = true;
+                if (user.CarType != i)
+                {
+                    EquipBtn.gameObject.SetActive(true);
+                }
+                break;
+            }
+        }
+        if (!b_Owned)
+        {
+            BuyBtn.gameObject.SetActive(true);
+        }
+        if (user.CarType == i)
+        {
+            Equiped.gameObject.SetActive(true);
+        }
+
+        switch (i)
+        {
+            case 0:
+                CarImage.GetComponent<Image>().sprite = Car1;
+                Diamondcost = 35;
+                break;
+            case 1:
+                CarImage.GetComponent<Image>().sprite = Car2;
+                Diamondcost = 45;
+                break;
+            case 2:
+                CarImage.GetComponent<Image>().sprite = Car3;
+                Diamondcost = 55;
+                break;
+            case 3:
+                CarImage.GetComponent<Image>().sprite = Car4;
+                Diamondcost = 205;
+                break;
+        }
+        if (BuyBtn.enabled)
+            SetTextColor();
+    }
+    public void SetTextColor()
+    {
+        if (user.DiamondCount >= Diamondcost)
+            BuyText.color = Color.blue;
+        else if (user.DiamondCount < Diamondcost)
+            BuyText.color = Color.red;
+    }
+
+
+
+    }
--- Assets/Scripts/UserInfo.cs
+++ Assets/Scripts/UserInfo.cs
@@ -3,16 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class UserInfo : MonoBehaviour
+public class UserInfo
 {
     public string Username;
     public int Level = 1;
     public int DiamondCount = 199;
 
     //Car
-    public int CarType = 1;
-    public int WheelType = 1;
+    public int CarType = 0;
+    public int WheelType = 0;
+    public int EngineType = 0;
 
+    public int[] OwnedCar = {0,1, };
     //Setters
     public void SetUsername(string name)
     {
